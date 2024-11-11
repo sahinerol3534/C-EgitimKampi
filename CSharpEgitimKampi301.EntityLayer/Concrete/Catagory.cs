4a4790711efd5239795a00e3f1c3e53bdbcbf2a2
sahinerol3534 <serol3534@gmail.com>
2024-11-11 17:47:10 -0500

    Yeni Proje

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Catagory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Catagory
+    {
+        /* 
+         * field    ---> Bir değişken direkt olarak sınıfın içinde tanımlanırsa
+         * variable ---> Bir değişken bir metod içinde tanımlanıyorsa
+         * Property ---> Değişken yapısında sonuna {get; set;} yapısı alıyorsa
+         */
+
+        public int CatagoryId { get; set; }
+        /* birincil anahtar ve otomatik olarak artması için class'ın ismi aynı yazılıp sonuna 'Id' yazısı 
+         * konmalıdır
+        */
+        public string CatogoryName { get; set; }
+        /* 
+         * get veriyi alırken kısıt getirmek Örnek: CatogoryName i büyük harf olarak çekmek gibi
+         * set veri formunu ayarlamak için. örnek: verinin karakter uzunluğunu 5 karakterden küçük
+         * se kullanıcıya uyarı verilmesi.
+         */
+        public bool CatagoryStatus { get; set; }
+        public List<Product> products { get; set; }
+
+    }
+}
