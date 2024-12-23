a11eda978fa433c87b488cd8bd4470c83fcb17c6
sahinerol3534 <serol3534@gmail.com>
2024-12-22 22:00:30 -0500

    CSharpEgitimKampi501

--- CSharpEgitimKampi501/Form1.cs
+++ CSharpEgitimKampi501/Form1.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using CSharpEgitimKampi501.Dtos;
+using Dapper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,23 +19,41 @@ namespace CSharpEgitimKampi501
         {
             InitializeComponent();
         }
+        SqlConnection connection = new SqlConnection("server = SAHINEROL; initial Catalog = EgitimKampi501Db; integrated security = true ");
 
-        private void button3_Click(object sender, EventArgs e)
+        private async void btnlist_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM TblProduct";
+            var values = await connection.QueryAsync<ResultProductDto>(query);
+            dataGridView1.DataSource = values;
+
+        }
+
+        private async void btnAdd_Click(object sender, EventArgs e)
+        {
+            // SQL sorgusunda @productPrice parametresinin doğru şekilde kullanılması
+            string query = "INSERT INTO TblProduct(ProductName, ProductStock, ProductPrice, ProductCatagory) values(@productName, @productStock, @productPrice, @productCatagory)";
+
+            // Parametreleri dinamik olarak ekliyoruz
+            var parameters = new DynamicParameters();
+            parameters.Add("@productName", txtProductName.Text);
+            parameters.Add("@productStock", txtProductStock.Text);
+            parameters.Add("@productPrice", txtProductPrice.Text);
+            parameters.Add("@productCatagory", txtProductCatagory.Text);
+
+            // Sorguyu asenkron olarak çalıştırıyoruz
+            await connection.ExecuteAsync(query, parameters);
+
+            // Kullanıcıya işlem sonucu hakkında bilgi veriyoruz
+            MessageBox.Show("Yeni Kitap Eklendi.");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void btnUpdate_Click(object sender, EventArgs e)
         {
 
         }
